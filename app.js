fe00fd7a2f1feffe89ac7abab4ac077a166bad1e
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-13 13:41:51 -0500

    How to Parse JSON

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const https = require("https");
 require("dotenv").config();
-console.log(process.env);
+
 
 const app = express();
 const port = 3000;
@@ -9,10 +9,19 @@ const port = 3000;
 
 app.get("/", (req, res) => {
     const api_key = process.env.API_KEY;
-    const url  = "https://api.openweathermap.org/data/2.5/weather?q=Brownsville,us&appid=${api_key}&units=metric";
+    const url  = "https://api.openweathermap.org/data/2.5/weather?q=Brownsville,us&appid=" + api_key + "&units=metric";
     
     https.get(url, (response) => {
-        console.log(response);
+        console.log(response.statusCode);
+        //using ".on()"" method to get Api data and then parsing the code through JSON ".parse()"" method
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            // console.log(weatherData);
+            const temp = weatherData.main.temp;
+            // console.log(temp);
+            const description = weatherData.weather[0].description
+            console.log(description);
+        });
     });
     
     res.send("Server Runnig")
