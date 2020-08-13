18cd0c58f7ae530507cabb2ed3ec48a8fc904952
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-13 14:19:22 -0500

    Use Express to Render Website with Live API Data

--- app.js
+++ app.js
@@ -6,25 +6,27 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
-
+//res refers to app.get response
 app.get("/", (req, res) => {
     const api_key = process.env.API_KEY;
     const url  = "https://api.openweathermap.org/data/2.5/weather?q=Brownsville,us&appid=" + api_key + "&units=metric";
     
+    //response refers to https.get response
     https.get(url, (response) => {
         console.log(response.statusCode);
         //using ".on()"" method to get Api data and then parsing the code through JSON ".parse()"" method
         response.on("data", (data) => {
             const weatherData = JSON.parse(data);
-            // console.log(weatherData);
             const temp = weatherData.main.temp;
-            // console.log(temp);
             const description = weatherData.weather[0].description
-            console.log(description);
+            const icon = weatherData.weather[0].icon;
+            const imageURL =  "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p></p>The weather is currently " + description + "</p>");
+            res.write("<h1>The temperature in Brownsville is " + temp + " degrees Celsius</h1>");
+            res.write("<img src=" + imageURL + ">" );
+            res.send();
         });
     });
-    
-    res.send("Server Runnig")
 });
 
 
