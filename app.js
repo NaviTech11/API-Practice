f590bf8197a314afe4ca9aa01e038b48ea6f66a0
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-13 12:40:35 -0500

    Hide Api Key

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const https = require("https");
+require("dotenv").config();
+console.log(process.env);
 
 const app = express();
 const port = 3000;
 
 
 app.get("/", (req, res) => {
+    const api_key = process.env.API_KEY;
+    const url  = "https://api.openweathermap.org/data/2.5/weather?q=Brownsville,us&appid=${api_key}&units=metric";
     
-    https.get("https://api.openweathermap.org/data/2.5/weather?q=Brownsville,us&appid=50cd2b33f6bd5eba764b335cc0c943de&units=metric")
+    https.get(url, (response) => {
+        console.log(response);
+    });
+    
+    res.send("Server Runnig")
 });
 
 
