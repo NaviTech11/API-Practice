331a7567164fd736e9ed86c27aa601ce47b11239
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-13 15:09:30 -0500

    Use Body Parser to Parse POST Requests to the Server

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 //res refers to app.get response
 app.get("/", (req, res) => {
-    const api_key = process.env.API_KEY;
-    const url  = "https://api.openweathermap.org/data/2.5/weather?q=Brownsville,us&appid=" + api_key + "&units=metric";
-    
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body.cityName);
+    const city = req.body.cityName;
+    const country = "us";
+    const apiKey = process.env.API_KEY;
+    const unit = "metric";
+    const url  = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + apiKey + "&units=" + unit;
+
     //response refers to https.get response
     https.get(url, (response) => {
         console.log(response.statusCode);
@@ -22,12 +34,14 @@ app.get("/", (req, res) => {
             const icon = weatherData.weather[0].icon;
             const imageURL =  "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p></p>The weather is currently " + description + "</p>");
-            res.write("<h1>The temperature in Brownsville is " + temp + " degrees Celsius</h1>");
+            res.write("<h1>The temperature in " + city + " is " + temp + " degrees Celsius</h1>");
             res.write("<img src=" + imageURL + ">" );
             res.send();
         });
     });
 });
+
+
 
 
 app.listen(port, () => {
